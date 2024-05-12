6d7a343a3f30022a61a60de61805fd9a05671360
Алексей <akarpushev@yandex.ru>
2024-05-12 22:23:02 +0300

    работа с калькулятором

--- scripts/practice_2/a_Повторение/calculator_main.py
+++ scripts/practice_2/a_Повторение/calculator_main.py
@@ -9,9 +9,26 @@ class Window(QtWidgets.QWidget):
 
         self.ui.n1.clicked.connect(lambda: self.input("1"))
         self.ui.n2.clicked.connect(lambda: self.input("2"))
+        self.ui.n3.clicked.connect(lambda: self.input("3"))
+        self.ui.n4.clicked.connect(lambda: self.input("4"))
+        self.ui.n5.clicked.connect(lambda: self.input("5"))
+        self.ui.n6.clicked.connect(lambda: self.input("6"))
+        self.ui.n7.clicked.connect(lambda: self.input("7"))
+        self.ui.n8.clicked.connect(lambda: self.input("8"))
+        self.ui.n9.clicked.connect(lambda: self.input("9"))
+        self.ui.n0.clicked.connect(lambda: self.input("0"))
+        #self.pushButton_add.pressed.connect(lambda: self.operation(operator.add))
+        self.ui.plus.connect(lambda: self.add())
+
+
+
 
     def input(self, number):
         self.ui.lineEdit.setText(self.ui.lineEdit.text() + number)
+
+    def add(self):
+        self.t = "+"
+        self.label.setText(str(self.ans))
 
 
 
