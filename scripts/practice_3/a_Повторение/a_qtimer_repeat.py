62939ede67805bf9244b55495e18893c398f46ca
akarpushev <akarpushev@yandex.ru>
2024-05-16 21:01:54 +0300

    16.05 практика 2

--- scripts/practice_3/a_Повторение/a_qtimer_repeat.py
+++ scripts/practice_3/a_Повторение/a_qtimer_repeat.py
@@ -7,13 +7,20 @@
 с некоторой периодичностью вызывать определённую функцию.
 """
 
-from PySide6 import QtWidgets
+from PySide6 import QtWidgets, QtCore
+from time import sleep
 
 
 class Window(QtWidgets.QWidget):
 
     def __init__(self, parent=None):
         super().__init__(parent)
+        self.timer = QtCore.QTimer()
+        self.timer.setInterval(1000)
+        self.timer.timeout.connect(lambda: print("Прошла секунда"))
+        #self.timer.timeout.connect(self.long_proc)
+        self.timer.start()
+
 
 
 if __name__ == "__main__":
--- scripts/practice_3/a_Повторение/b_qthread_repeat.py
+++ scripts/practice_3/a_Повторение/b_qthread_repeat.py
@@ -14,13 +14,44 @@
 Опционально поработать с валидацией url
 """
 
-from PySide6 import QtWidgets
+from PySide6 import QtWidgets, QtCore
+from time import sleep
+
+class Worker(QtCore.QThread):
+    progress = QtCore.Signal(list)
+    def run(self) -> None:
+        for i in range(10):
+            sleep(1)
+            print(i)
+            self.progress.emit([1])
 
 
 class Window(QtWidgets.QWidget):
 
     def __init__(self, parent=None):
         super().__init__(parent)
+        self.setGeometry(0, 0, 300, 300)
+        self.label = QtWidgets.QLabel("Текст")
+        self.push = QtWidgets.QPushButton("Нажми")
+        layout = QtWidgets.QVBoxLayout(self)
+        layout.addWidget(self.label)
+        layout.addWidget(self.push)
+
+        self.work = Worker()
+        self.push.clicked.connect(self.start_thread)
+        self.work.progress.connect(self.set_data_in_label([0]))
+        #self.work.progress.connect(lambda data: self.label.setText(str(data[0])))
+
+    def start_thread(self):
+        self.work.start()
+
+    def set_data_in_label(self, data):
+        self.label.setText(str(data[0]))
+
+
+
+
+
 
 
 if __name__ == "__main__":
